a32f074c73b5fce3e1afd61741b93667b5c5fe02
Jay Beavers <jay@proxim.ly>
2014-12-07 16:41:21 -0500

    Add XBox controller placeholder code

--- tankBean.js
+++ tankBean.js
@@ -12,6 +12,10 @@ serialPort.on('data', function(error, data ) {
 	console.log(data);
 });
 
+serialPort.on('error', function(error, data ) {
+  console.log(data);
+});
+
 var stdin = process.stdin;
 stdin.setRawMode( true );
 stdin.resume();
@@ -26,11 +30,22 @@ stdin.on( 'data', function( key ){
   process.stdout.write( key );
 
   serialPort.write(key, function(err, results) {
-  	
+
   	if (err) {
 	    console.log('err ' + err);
 	}
 
   });
 
+});
+
+var XboxController = require('xbox-controller');
+var xbox = new XboxController('Microsoft Xbox Wired Gamepad');
+
+xbox.on('left:move', function(position){
+  console.log('left:move', position);
+});
+
+xbox.on('right:move', function(position){
+  console.log('right:move', position);
 });
